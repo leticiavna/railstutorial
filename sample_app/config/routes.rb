b1f36bce82aa58824a4fbb9799948451fde3d286
Letícia Vidal <leticiavna@id.uff.br>
2018-04-26 13:32:22 -0300

    third app - finish layout and routes

---
+++ sample_app/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # root page
+  root 'static_pages#home'
+
+  # static pages routes
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+
+  #users routes
+  get  '/signup',  to: 'users#new'
+end
